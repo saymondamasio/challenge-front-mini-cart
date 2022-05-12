19653859c64f0baa432995452f46f5b442f67169
Saymon Damásio <saymon.damasio95@gmail.com>
2022-05-12 14:25:55 -0300

    feat: add menu mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,30 @@
 import styles from './styles.module.scss'
 
-import { MdPerson, MdSearch, MdShoppingCart } from 'react-icons/md'
+import {
+  MdClose,
+  MdMenu,
+  MdPerson,
+  MdSearch,
+  MdShoppingCart,
+} from 'react-icons/md'
 import { useState } from 'react'
 import { Cart } from '../Cart'
+import { useMediaQuery } from '../../hooks/media'
+import { HeaderMenu } from './HeaderMenu'
 
 export function Header() {
   const [isOpenCart, setIsOpenCart] = useState(false)
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
+  const isMediumScreen = useMediaQuery('(max-width: 990px)')
+  const isSmallScreen = useMediaQuery('(max-width: 540px)')
+
+  function handleToggleMenu() {
+    if (!isOpenMenu && isOpenCart) setIsOpenCart(false)
+    setIsOpenMenu(!isOpenMenu)
+  }
 
   function handleOpenCart() {
+    if (!isOpenCart && isOpenMenu) setIsOpenMenu(false)
     setIsOpenCart(!isOpenCart)
   }
 
@@ -16,37 +33,32 @@ export function Header() {
       <div className={styles.container}>
         <img src="/images/logo.png" alt="Logo Agencia E-Plus" />
 
-        <nav>
-          <ul>
-            <li>
-              <a href="#">Lorem ipsum</a>
-            </li>
-            <li>
-              <a href="#">Lorem ipsum</a>
-            </li>
-            <li>
-              <a href="#">Lorem ipsum</a>
-            </li>
-            <li>
-              <a href="#">Lorem ipsum</a>
-            </li>
-            <li>
-              <a href="#">Lorem ipsum</a>
-            </li>
-          </ul>
-        </nav>
+        {!isMediumScreen && <HeaderMenu />}
 
         <div className={styles.actions}>
-          <MdSearch size={30} color="#000" />
-          <MdPerson size={30} color="#000" />
+          <MdSearch size={isSmallScreen ? 25 : 30} color="#000" />
+          <MdPerson size={isSmallScreen ? 25 : 30} color="#000" />
           <div className={styles.buttonCartWrapper}>
             <button type="button" onClick={handleOpenCart}>
-              <MdShoppingCart size={30} color="#000" />
+              <MdShoppingCart size={isSmallScreen ? 25 : 30} color="#000" />
             </button>
             {isOpenCart && <Cart />}
           </div>
+          {isMediumScreen && (
+            <>
+              <button type="button" onClick={handleToggleMenu}>
+                {isOpenMenu ? (
+                  <MdClose size={isSmallScreen ? 25 : 30} color="#000" />
+                ) : (
+                  <MdMenu size={isSmallScreen ? 25 : 30} color="#000" />
+                )}
+              </button>
+            </>
+          )}
         </div>
       </div>
+
+      {isMediumScreen && isOpenMenu && <HeaderMenu />}
     </header>
   )
 }
