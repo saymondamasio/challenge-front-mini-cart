3c3bdfc334d08a487f86c2f7112318f99d54688b
Saymon Damásio <saymon.damasio95@gmail.com>
2022-05-12 14:23:02 -0300

    refactor: componetizing menu navigation

---
+++ src/components/Header/HeaderMenu/index.tsx
@@ -0,0 +1,25 @@
+import styles from './styles.module.scss'
+
+export function HeaderMenu() {
+  return (
+    <nav className={styles.container}>
+      <ul>
+        <li>
+          <a href="#">Lorem ipsum</a>
+        </li>
+        <li>
+          <a href="#">Lorem ipsum</a>
+        </li>
+        <li>
+          <a href="#">Lorem ipsum</a>
+        </li>
+        <li>
+          <a href="#">Lorem ipsum</a>
+        </li>
+        <li>
+          <a href="#">Lorem ipsum</a>
+        </li>
+      </ul>
+    </nav>
+  )
+}
